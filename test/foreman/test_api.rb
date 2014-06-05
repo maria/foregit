b8be8ddd518ee4426c3ccf1ff7034fa95e8dfc61
Maria Nita <maria.nita.dn@gmail.com>
2014-06-05 23:12:59 +0300

    Add test directory and API test scaffold

---
+++ test/foreman/test_api.rb
@@ -0,0 +1,12 @@
+# Test the API binding to Foreman instance
+#
+require 'rspec'
+require_relative '../../lib/foreman/api'
+
+describe Foreman::Api do
+
+  describe '#initialize' do
+    puts 'Test'
+  end
+
+end
