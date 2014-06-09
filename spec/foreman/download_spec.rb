9f4149c826edca7bc0bcf8acc8ca41ba9b9c0682
Maria Nita <maria.nita.dn@gmail.com>
2014-06-09 23:28:25 +0300

    Mock Foreman API response #7

--- spec/foreman/download_spec.rb
+++ spec/foreman/download_spec.rb
@@ -1,24 +1,26 @@
 # Test the API binding to Foreman instance
-#
+#ec'
+require 'json'
 require 'rspec'
 
-require_relative '../../lib/foregit'
-require_relative '../../lib/settings'
+require_relative '../../lib/foreman/api'
 require_relative '../../lib/foreman/download'
 
 describe Foreman::Download do
 
   before (:each) do
+    @api = Foreman::Api.api
+    allow(@api).to receive(:call).and_return(build(:resources).to_json)
+
     @binding = Foreman::Download.new
     @downloaded_resources = @binding.download_resources
   end
 
   describe '#download_resources' do
     context 'download resources defined in settings file' do
-        subject(:downloaded_resources) {@downloaded_resources}
-            it 'should return a hash with :architectures' do
-                expect(downloaded_resources.is_a? Hash).to be(true)
-            end
+      it 'should return a hash with :architectures' do
+        expect(@downloaded_resources.is_a? Hash).to be(true)
+      end
     end
   end
 
