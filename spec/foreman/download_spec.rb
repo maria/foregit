7ad97dcded3bac743609aa9f78811a84c5a6dca0
Maria Nita <maria.nita.dn@gmail.com>
2014-06-12 09:17:48 +0300

    Update assertion. #7

--- spec/foreman/download_spec.rb
+++ spec/foreman/download_spec.rb
@@ -1,4 +1,4 @@
-# Test the API binding to Foreman instance
+# Test the API binding used to download Foreman instance
 require 'json'
 
 require 'rspec'
@@ -25,24 +25,24 @@ describe Foreman::Download do
     context 'download resources defined in settings file' do
       it 'should return a hash with :architectures' do
         downloaded_resources = @binding.download_resources
-        expect(downloaded_resources.is_a? Hash).to be(true)
-        expect(downloaded_resources.has_key? :architectures).to be(true)
+        expect(downloaded_resources).to be_an_instance_of(Hash)
+        expect(downloaded_resources).to have_key(:architectures)
       end
     end
 
     context 'download resources passed as arguments' do
       it 'should return a hash with :hosts_groups' do
         downloaded_resources = @binding.download_resources(:hosts_groups)
-        expect(downloaded_resources.is_a? Hash).to be(true)
-        expect(downloaded_resources.has_key? :hosts_groups).to be(true)
+        expect(downloaded_resources).to be_an_instance_of(Hash)
+        expect(downloaded_resources).to have_key(:hosts_groups)
       end
     end
 
     context 'download resources raises error when no resource is set' do
-      it 'should raise error' do
+      it 'should raise ArgumentError' do
         # Ensure no resources are set to be downloaded from Foreman
         Foregit::SETTINGS.delete(:resources)
-        expect{@binding.download_resources}.to raise_error
+        expect{@binding.download_resources}.to raise_error(ArgumentError)
       end
     end
 
