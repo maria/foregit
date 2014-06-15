c7b180fac2a78b4833a2cc0517afe97dfc59c850
Maria Nita <maria.nita.dn@gmail.com>
2014-06-16 01:20:07 +0300

    Enhace dump object as file method #6

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'json'
 
 require 'foregit'
@@ -34,11 +35,36 @@ class FileManager
     end
   end
 
-  def dump_object_in_file(resource, file)
-    file_path = find_file(file)
+  def dump_object_as_file(resource, file=nil)
+    if !file.nil?
+      file_path = find_file(file)
+
+    elsif file.nil?
+      # If we don't have a file, it means it's a new configuration and a new
+      # file has to be created beforehand.
+      dir_path = ensure_directory(resource[:type])
+      file_path = ensure_file(dir_path, resource[:name])
+    end
+
     opened_file = File.open(file_path, 'w')
-    object = opened_file.write(JSON.dump(resource))
+    object = opened_file.write(JSON.dump(resource[:content]))
     opened_file.close
+  end
+
+  def ensure_directory(directory)
+    dir_path = File.join(@repo_path, directory)
+    if !Dir.exists?(dir_path)
+      FileUtils.mkdir(dir_path)
+    end
+    return dir_path
+  end
+
+  def ensure_file(directory, file, extension='.json')
+    file_path = File.join(directory, file + extension)
+    if !File.exists?(file_path)
+      FileUtils.touch(file_path)
+    end
+    return file_path
   end
 
   def can_read_file?(file)
