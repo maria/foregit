d351e891f06554207fd818394afea08a88dde181
Maria Nita <maria.nita.dn@gmail.com>
2014-06-15 22:50:28 +0300

    Write test cases for FileManager #6

--- spec/unit/file_manager_spec.rb
+++ spec/unit/file_manager_spec.rb
@@ -1,21 +1,30 @@
 require 'spec_helper'
 
+require 'foregit'
 require 'file_manager'
 
 
 describe FileManager do
 
-  before (:each) do
-  end
-
   describe '#initiliaze' do
+
     it 'should set repo_path when is set in settings' do
+      repo_path = '/home/test/repo/path'
+      Foregit::SETTINGS[:repo_path] = repo_path
+      manager = FileManager.new
+      expect(manager.repo_path).to be(repo_path)
     end
 
     it 'should set repo_path when is given' do
+      repo_path = '/home/test/repo/path'
+      Foregit::SETTINGS[:repo_path] = '/home/test/false/repo/path'
+      manager = FileManager.new repo_path
+      expect(manager.repo_path).to be(repo_path)
     end
 
     it 'should raise an error when no repo_path is set' do
+      Foregit::SETTINGS[:repo_path] = nil
+      expect{FileManager.new}.to raise_error(ArgumentError)
     end
 
   end
@@ -23,10 +32,29 @@ describe FileManager do
   describe '#find_file' do
     context 'file path when the file is inside the given repo path' do
 
+      repo_path = '/home/test/repo/path'
+      manager = FileManager.new(repo_path)
+
       it 'should return the path when we pass the full file path' do
+        file = '/test'
+        file_path = repo_path + file
+
+        expect(FileManager::File).to receive(:expand_path).with(file).and_return(file_path)
+        expect(FileManager::File).to receive(:fnmatch?).with(repo_path + '*', file_path).and_return(true)
+        expect(manager).to receive(:can_read_file?).with(file_path).and_return(true)
+
+        expect(manager.find_file file).to match(file_path)
       end
 
       it 'should return the path when we pass the relative path of the file within the repo' do
+        file = '../test'
+        file_path = '/home/test/repo/test'
+
+        expect(FileManager::File).to receive(:expand_path).with(file).and_return(file_path)
+        expect(FileManager::File).to receive(:fnmatch?).with(repo_path + '*', file_path).and_return(true)
+        expect(manager).to receive(:can_read_file?).with(file_path).and_return(true)
+
+        expect(manager.find_file file).to match(file_path)
       end
     end
 
