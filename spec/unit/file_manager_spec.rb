f8c6c2639d03690d008ed08243293a7dd39d9176
Maria Nita <maria.nita.dn@gmail.com>
2014-06-16 16:20:20 +0300

    Refactor tests #6

--- spec/unit/file_manager_spec.rb
+++ spec/unit/file_manager_spec.rb
@@ -7,38 +7,44 @@ require 'file_manager'
 
 describe FileManager do
 
+  before(:all) do
+    FileUtils.mkdir(File.expand_path('../data', __FILE__))
+  end
+
+  after(:all) do
+    FileUtils.rm_rf(File.expand_path('../data', __FILE__))
+  end
+
   describe '#initiliaze' do
+    let(:repo_path) { File.expand_path('../data', __FILE__) }
 
     it 'should set repo_path when is set in settings' do
-      repo_path = '/home/test/repo/path'
       Foregit::SETTINGS.repo_path = repo_path
       manager = FileManager.new
       expect(manager.repo_path).to be(repo_path)
     end
 
     it 'should set repo_path when is given' do
-      repo_path = '/home/test/repo/path'
-      Foregit::SETTINGS.repo_path = '/home/test/false/repo/path'
+      Foregit::SETTINGS.repo_path = repo_path
       manager = FileManager.new repo_path
       expect(manager.repo_path).to be(repo_path)
     end
 
     it 'should raise an error when no repo_path is set' do
       Foregit::SETTINGS.repo_path = nil
-      expect{FileManager.new}.to raise_error(ArgumentError)
+      expect{ FileManager.new }.to raise_error(ArgumentError)
     end
 
   end
 
   describe '#find_file' do
     context 'file path when the file is inside the given repo path' do
-
-      repo_path = '/home/test/repo/path'
-      manager = FileManager.new(repo_path)
+      let(:repo_path) { File.expand_path('../data', __FILE__) }
+      let(:manager) { FileManager.new repo_path }
 
       it 'should return the path when we pass the full file path' do
         file = '/test'
-        file_path = repo_path + file
+        file_path = File.join(repo_path, file)
 
         expect(FileManager::File).to receive(:expand_path).with(file).and_return(file_path)
         expect(FileManager::File).to receive(:fnmatch?).with(repo_path + '*', file_path).and_return(true)
@@ -49,7 +55,7 @@ describe FileManager do
 
       it 'should return the path when we pass the relative path of the file within the repo' do
         file = '../test'
-        file_path = '/home/test/repo/test'
+        file_path = File.join(repo_path, file)
 
         expect(FileManager::File).to receive(:expand_path).with(file).and_return(file_path)
         expect(FileManager::File).to receive(:fnmatch?).with(repo_path + '*', file_path).and_return(true)
@@ -60,12 +66,12 @@ describe FileManager do
     end
 
     context 'file path when the file is not inside the given repo' do
-      repo_path = '/home/test/repo/path'
-      manager = FileManager.new(repo_path)
+      let(:repo_path) { File.expand_path('../data', __FILE__) }
+      let(:manager) { FileManager.new repo_path }
 
       it 'should return nil we pass the path of a file outside the repo' do
         file = 'test'
-        file_path = '/home/test/repo/path/test'
+        file_path = File.join(repo_path, file)
 
         expect(FileManager::File).to receive(:expand_path).with(file).and_return(file_path)
         expect(FileManager::File).to receive(:join).with(repo_path, file).and_return(file_path)
@@ -80,42 +86,43 @@ describe FileManager do
   describe '#dump_object_as_file' do
 
     context 'dumping a valid JSON in an existent file' do
-      it 'the file should contain the resource content' do
+      let(:repo_path) { File.expand_path('../data', __FILE__) }
+      let(:manager) { FileManager.new repo_path }
 
-        repo_path = Dir.pwd
+      it 'the file should contain the resource content' do
         file = 'test.json'
-        file_path = repo_path + '/' + file
+        file_path = File.join(repo_path, file)
         File.open(file_path, 'w') {}
 
-        manage = FileManager.new(repo_path)
-        resource = {:content => {:resources => [:architectures, :hosts_groups]}}
+        resource = {
+          :type => 'resources',
+          :name => 'all',
+          :content => {:resources => [:architectures, :hosts_groups]}
+        }
 
-        expect(manage.dump_object_as_file(resource, file)).to be(nil)
+        manager.dump_object_as_file(resource, file)
         expect(File.read(file_path)).to match(resource[:content].to_json)
 
-        File.delete(file_path)
       end
     end
 
     context 'dumping a valid JSON in an inexistent file' do
+      let(:repo_path) { File.expand_path('../data', __FILE__) }
+      let(:manager) { FileManager.new repo_path }
+
       it 'the file should contain the resource content after is created' do
-        repo_path = Dir.pwd
         architectures = build(:architectures)
-        manage = FileManager.new(repo_path)
         resource = {
             :type => 'architectures',
             :name => architectures.name,
             :content => {:resources => [:architectures, :hosts_groups]}
         }
 
-        expect(manage.dump_object_as_file(resource)).to match(nil)
-
-        file_path = manage.find_file('architectures/' + architectures.name + '.json')
+        manager.dump_object_as_file(resource)
+        file_path = manager.find_file('architectures/' + architectures.name + '.json')
 
         expect(File.read(file_path)).to match(resource[:content].to_json)
 
-        File.delete(file_path)
-        Dir.delete(File.dirname(file_path))
       end
     end
 
