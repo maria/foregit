917b8a8a8d2140a5d7c4b2db918ffacc772f6bb8
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 10:36:06 +0300

    Add sync command - first iteration #8

---
+++ lib/sync.rb
@@ -0,0 +1,34 @@
+=begin
+sync command
+    - get settings
+    - download(settings[resources])
+    - file manager
+=end
+
+require 'foregit'
+require 'file_manager'
+require 'foreman/api'
+require 'foreman/download'
+
+class Sync
+
+  def initialize
+    @api = Foreman::Api.api
+  end
+
+  def sync_foreman_to_git
+    download_manager = Foreman::Download.new(@api)
+    resources = download_manager.download_resources
+    file_manager = FileManager.new
+
+    resources.each do |resource_type, resources_content|
+      resources_content.each do |resource_content|
+        resource = {:type => resource_type.to_s, :name => resource_content["name"].to_s,
+                    :content => resource_content}
+        puts resource
+        file_manager.dump_object_as_file(resource)
+      end
+    end
+  end
+
+end
