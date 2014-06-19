537485e0d2580c57b4b9324feb3d60f0d5382ce2
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 11:29:59 +0300

    Include all lib files inside the main module #8

--- lib/foregit.rb
+++ lib/foregit.rb
@@ -1,6 +1,13 @@
 module Foregit
 
   require 'settings'
+  require 'file_manager'
+  require 'sync'
+  require 'command'
+
+  require 'foreman/api'
+  require 'foreman/download'
+
   SETTINGS = Settings.load_from_file
 
 end
