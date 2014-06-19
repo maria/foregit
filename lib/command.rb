1aebdf87d78d6cfee69aaa155538ced348ddb651
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 11:01:40 +0300

    Add command file, to parse CLI arguments and run commands #8

---
+++ lib/command.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+require 'optparse'
+
+require 'sync'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: command.rb [options]"
+
+  opts.on("-s", "--sync", "Sync resources") do |s|
+    options[:sync] = s
+  end
+end.parse!
+
+
+if options.has_key? :sync
+    puts "Sync Foreman resources..."
+    sync = Sync.new
+    sync.sync_foreman_to_git
+    puts "Done!"
+end
