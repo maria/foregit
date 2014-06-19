e80c872fba83607bd02e97abe88cddd7cff27007
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 10:59:25 +0300

    Remove debug line, update style #8

--- lib/sync.rb
+++ lib/sync.rb
@@ -23,12 +23,14 @@ class Sync
 
     resources.each do |resource_type, resources_content|
       resources_content.each do |resource_content|
-        resource = {:type => resource_type.to_s, :name => resource_content["name"].to_s,
-                    :content => resource_content}
-        puts resource
+
+        resource = {:type => resource_type.to_s,
+                    :name => resource_content["name"].to_s,
+                    :content => resource_content
+                  }
+
         file_manager.dump_object_as_file(resource)
       end
     end
   end
-
 end
