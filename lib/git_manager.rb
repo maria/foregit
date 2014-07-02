2900096070fde9640c0ca0e141df52f1765e8e4c
Maria Nita <maria.nita.dn@gmail.com>
2014-07-02 12:57:38 +0300

    Get commits to check for changes. Update local repo.Get changed files #5

--- lib/git_manager.rb
+++ lib/git_manager.rb
@@ -5,15 +5,31 @@ require 'foregit'
 class GitManager
 
     def initialize
-      repo_path = Foregit::SETTINGS.repo_path
-      @git = Git.open(repo_path)
+      @git = Git.open(Foregit::SETTINGS.repo_path)
     end
 
-    def diff:
-      if !@git.status.untracked.empty?
-        @git.status.untracked.each do |file|
-            file_name = file[:name]
-        end
+    def get_diff
+      # get last local commit
+      last_commit_sha = @git.log(1)[0].sha
+      # ensure we are on the correct branch
+      if @git.branch.name != Foregit::SETTINGS.repo_branch
+        puts "Currently on #{@git.branch.name}, not on #{Foregit::SETTINGS.repo_branch}!"
+      end
+      # Get upstream changes
+      @git.pull
+      # Get differences between commit
+      new_last_commit_sha = @git.log(1)[0].sha
+      commits_diff = @git.diff(last_commit_sha, new_last_commit_sha).path('resources/')
+    end
+
+    def apply_diff(commits_diff)
+      if commits_diff.size == 0
+        puts 'Everything is up to date!'
+      end
+      # Get changes and apply them to Foreman instance
+      commits_diff.stats[:files].each do |file, stats|
+        puts file
       end
     end
+
 end
