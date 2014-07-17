10491cc85e42600ecac4a5005322fc5972223350
Maria Nita <maria.nita.dn@gmail.com>
2014-07-17 18:17:34 +0300

    Remove custom settings, use hammer-cli config file #13

--- lib/foregit.rb
+++ lib/foregit.rb
@@ -1,12 +1,9 @@
 module Foregit
 
-  require 'settings'
   require 'file_manager'
   require 'git_manager'
   require 'talk_commands'
 
   require 'foreman/api'
-
-  SETTINGS = Settings.load_from_file
 
 end
