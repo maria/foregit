890c6092244d00b20588f2e22fc6e201e89dc868
Maria Nita <maria.nita.dn@gmail.com>
2014-07-19 10:43:16 +0300

    Add option and define pull command #13

--- lib/hammer_cli_foregit/pull.rb
+++ lib/hammer_cli_foregit/pull.rb
@@ -1,11 +1,18 @@
 require 'hammer_cli'
 
+require 'talk_commands'
+
 module HammerCLIForegit
 
   class Pull < HammerCLI::AbstractCommand
 
+    option ['-r', '--resources'], 'RESOURCES', 'A list of resources to pull and save',
+      :format => HammerCLI::Options::Normalizers::List.new
+
     def execute
-      print_message "Hello World!"
+      talk = TalkCommands.new
+      talk.pull option_resources
+      HammerCLI::EX_OK
     end
 
   end
