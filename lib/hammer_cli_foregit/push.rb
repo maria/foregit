17efd4334fc1bf5728425f4c91f5682ece9b725e
Maria Nita <maria.nita.dn@gmail.com>
2014-07-25 00:40:34 +0300

    For each directory/resource load files as json and make a post request to Foreman #15

--- lib/hammer_cli_foregit/push.rb
+++ lib/hammer_cli_foregit/push.rb
@@ -8,9 +8,14 @@ module HammerCLIForegit
       super
       puts 'Upload changes to Foreman...'
       @git_manager.commit("Commit existent changes before push.")
-      @file_manager.get_repo_json_files.each do |file|
-        data = @file_manager.load_file_as_json(file)
-        @binding.call_action(file, :create, data)
+
+      @file_manager.get_repo_directories.each do |dir|
+        @file_manager.get_dir_json_files(dir).each do |file|
+
+          data = @file_manager.load_file_as_json(File.join(dir, file))
+          @binding.call_action(dir, :create, data)
+
+        end
       end
       puts 'Done!'
       HammerCLI::EX_OK
