5b235de76e5c900db04b795dc4ace4b4c0570dcc
Maria Nita <maria.nita.dn@gmail.com>
2014-07-25 10:32:19 +0300

    Include Talk, File and Git managers under Foregit module #13

--- lib/git_manager.rb
+++ lib/git_manager.rb
@@ -3,7 +3,9 @@ require 'git'
 
 require 'foregit'
 
-class GitManager
+module Foregit
+
+  class GitManager
 
     # Ensure the repository exists on the localhost. Either clone or create it.
     # Configure Git settings and ensure the workspace is on the expected branch.
@@ -66,5 +68,5 @@ class GitManager
       end
       commits_diff.stats[:files]
     end
-
+  end
 end
