da5bb5d30ca94b39f4a425e4c4a2b1ede83021a8
Maria Nita <maria.nita.dn@gmail.com>
2014-11-01 16:56:31 +0100

    Fix typos

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Instal cURL and RVM"
+echo "Installing cURL and RVM"
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y curl
 curl -L https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
 
-echo "Install Ruby and Rails"
+echo "Installing Ruby and Rails"
 rvm use --install 2.0.0-p594
 gem install rails -v 4.0.0 --no-ri --no-rdoc
 
-echo "Install Puppet and Foreman"
+echo "Installing Puppet and Foreman"
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 
@@ -21,6 +21,7 @@ wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 sudo apt-get update && sudo apt-get install -y foreman-installer
 sudo foreman-installer
 
-echo "Install accessories"
-sudo apt-get install -y vim
-sudo apt-get install -y git
+echo "Installing accessories"
+sudo apt-get install -y vim git
+
+echo "Done."
